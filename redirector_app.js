0f7bcbadb51565f678e914d05b3f22ba446b100a
Coty Beasley <hi@coty.email>
2020-09-21 00:11:39 -0500

    Updating slug

--- redirector_app.js
+++ redirector_app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
    Format: year-trip-asset-number
    Example: 16ildv1
  */
-app.get('/16ildv1', function (req, res) {
+app.get('16ildv1', function (req, res) {
     res.redirect(302, 'https://vimeo.com/459800355');
 });
 
