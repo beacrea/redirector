02cc2539cdb77a90ef511d829b2e30be43abca10
Coty Beasley <hi@coty.email>
2020-09-22 00:14:40 -0500

    Added more video links

--- redirector_app.js
+++ redirector_app.js
@@ -28,6 +28,21 @@ app.get('/16ildv3', function (req, res) {
    res.redirect(301, 'https://vimeo.com/460022494');
 });
 
+// Loophead Lighthouse Video
+app.get('/16ildv4', function (req, res) {
+   res.redirect(301, 'https://vimeo.com/460370316');
+});
+
+// Ballinskelligs Abbey & Castle Video
+app.get('/16ildv5', function (req, res) {
+   res.redirect(301, 'https://vimeo.com/460411796');
+});
+
+// Muckross Abbey Video
+app.get('/16ildv6', function (req, res) {
+   res.redirect(301, 'https://vimeo.com/460431341');
+});
+
 // Port listening text
 app.listen(port, function () {
     console.log('Example app listening on port ' + port + '!');
